8a66a3361c25a385a8420b17204fda23d9d0cadc
drew <drewbeland@gmail.com>
2023-02-13 18:37:25 -0800

    testing controller

--- controller.js
+++ controller.js
@@ -1,2 +1,9 @@
+import wasmURL from './pkg/annoy_bg.wasm'
+import init, { load_index, IndexType } from "./pkg/annoy";
+
+const response = await fetch('./index.angular.5d.ann');
+const ab = await response.arrayBuffer();
+const u8a = Buffer.from(ab);
+this.index = load_index(u8a, 5, IndexType.angular);
+
 console.log('taco')
-loadIndex()
