86a6c9be4c965a3a359825a5aeda5e715fabb7aa
drew <drewbeland@gmail.com>
2023-02-18 15:26:28 -0800

    working on vector search

--- controller.js
+++ controller.js
@@ -7,14 +7,16 @@ const response = await fetch('./index.angular.5d.ann');
 const ab = await response.arrayBuffer();
 const u8a = new Uint8Array(ab)
 
+
+
 document.getElementById('loadIndex').addEventListener('click', initializeIndex)
 
 function initializeIndex(){
-let userInput = document.getElementById('dimensionInput').innerText
+    let dimensions = document.getElementById('dimensionInput').value
 
-// const u8a = Buffer.from(ab);
-let index = load_index(u8a, 5, IndexType.angular);
+    // const u8a = Buffer.from(ab);
+    let index = load_index(u8a, dimensions, IndexType.angular);
 
-console.log(index.get_nearest([ 1.0689810514450073, 0.5634735226631165, 0.24886439740657806, 0.7266523241996765, -0.646281898021698 ], 10, -1, true))
+    console.log(index)
 
 }
