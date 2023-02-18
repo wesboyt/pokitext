7c4cbc59670fb842f5568b4c1753e8f7344cdcf7
drew <drewbeland@gmail.com>
2023-02-18 12:48:11 -0800

    started adding functionality for searching

--- controller.js
+++ controller.js
@@ -7,7 +7,14 @@ const response = await fetch('./index.angular.5d.ann');
 const ab = await response.arrayBuffer();
 const u8a = new Uint8Array(ab)
 
+document.getElementById('submit').addEventListener('click', search)
+
+function search(){
+let userInput = document.getElementById('dimensionInput').innerText
+
 // const u8a = Buffer.from(ab);
 let index = load_index(u8a, 5, IndexType.angular);
 
 console.log(index.get_nearest([ 1.0689810514450073, 0.5634735226631165, 0.24886439740657806, 0.7266523241996765, -0.646281898021698 ], 10, -1, true))
+
+}
